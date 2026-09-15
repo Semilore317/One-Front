b0425d38c3881fe660ca0b5021612a5945f1b779
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-09-15 06:38:38 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <raylib.h>
+
+int main() {
+    InitWindow(1336, 768, "One Front");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(Color(22, 31, 40, 255));
+        DrawText("One Front", 30, 30, 32, RAYWHITE);
+
+        // temporary ground
+        DrawRectangle(0, 520, 1100, 100, DARKGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}

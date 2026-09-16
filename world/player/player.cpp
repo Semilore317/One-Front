8f72e1220d57e0d8959e2701ec5e7949661ef402
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-09-16 22:04:19 +0100

    feat: implemented basic gravity system and jumping controls

--- world/player/player.cpp
+++ world/player/player.cpp
@@ -1,25 +1,28 @@
 #include "player.hpp"
+#include "raylib.h"
 
-constexpr float DEFAULT_MOVE_SPEED = 200.0f; // 200px per second
+constexpr float DEFAULT_MOVE_SPEED = 400.0f; // 200px per second
+constexpr float GRAVITY = 600.0f;
+constexpr float JUMP_SPEED = 250.0f;
 
-Player::Player(): 
-    position{0, 0}, 
-    velocity{0, 0}, 
+Player::Player():
+    isGrounded{true},
+    position{0, 0},
+    velocity{0, 0},
     size{1, 1} {}
 
-Player::Player(Vector2 position, Vector2 size): 
-    position{position}, 
-    velocity{0, 0}, 
+Player::Player(Vector2 position, Vector2 size):
+    isGrounded{true},
+    position{position},
+    velocity{0, 0},
     size{size} {}
 
+void Player::apply_gravity(float deltaTime) {
+    velocity.y += GRAVITY * deltaTime;
+}
 
-void Player::update(float deltaTime, const Controls& controls) {
-  // locomotion lives here
-
-  // TODO: JUMP & GRAVITY
-  // pressing up is a jump and i haven't fully thought it out yet.
-  // would require a gravity mechanism
-
+void Player::update(float deltaTime, const Controls& controls, float groundY) {
+  // horizontal movement
   if(IsKeyDown(controls.left))
       velocity.x = -DEFAULT_MOVE_SPEED;
   else if(IsKeyDown(controls.right))
@@ -27,7 +30,29 @@ void Player::update(float deltaTime, const Controls& controls) {
   else
       velocity.x = 0;
 
+  // if jump was pressed AND is grounded
+  // velocity.y = -GRAVITY * deltaTime;
+  // isGrounded = false;
+
+  if(IsKeyPressed(controls.jump) && isGrounded){
+      velocity.y = -JUMP_SPEED;
+      isGrounded = false;
+  }
+
+  // if airborne
+  // apply gravity
+  if(!isGrounded)
+      apply_gravity(deltaTime);
+
   position.x += velocity.x * deltaTime;
+  position.y += velocity.y * deltaTime;
+
+  // check whether the bottom of the avatar has hit the ground
+  if(position.y + size.y >= groundY){
+      position.y = groundY - size.y;
+      velocity.y = 0;
+      isGrounded = true;
+  }
 }
 
 void Player::draw() const{

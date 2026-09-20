82b2001fe77ea9ea34c13f8c95c25e14b95cb732
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-09-20 18:00:29 +0100

    chore: more post-PR cleanup

--- world/platform/platform.hpp
+++ world/platform/platform.hpp
@@ -6,16 +6,8 @@ struct Platform {
 	Vector2 position;
 	Vector2 size;
 
-	float left() const {
-		return position.x;
-	}
-	float right() const {
-		return position.x + size.x;
-	}
-	float top() const {
-		return position.y;
-	}
-	float bottom() const {
-		return position.y + size.y;
-	}
+	float left() const { return position.x; }
+	float right() const { return position.x + size.x; }
+	float top() const { return position.y; }
+	float bottom() const { return position.y + size.y; }
 };

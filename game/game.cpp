30880c25e5d14518a4f2f32f0e5fc234816b10ec
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-09-20 18:26:06 +0100

    fix: game.cpp merge conflict

--- game/game.cpp
+++ game/game.cpp
@@ -37,29 +37,15 @@ void Game::draw() const {
 	for (const Platform &platform : world.platforms)
 		DrawRectangleV(platform.position, platform.size, BROWN);
 
-	/*
-	// TEMP: basic visualization until player renderer is merged
-	DrawRectangleV(player.position, player.size, RED);
-
-	float markerX = player.facing == Facing::Right
-	    ? player.position.x + player.size.x - 5
-	    : player.position.x;
-
-	DrawRectangle(markerX, player.position.y + 10, 5, 10, WHITE);
-	*/
-	// 1. define health bar dimensions and screen positions within game
 	float barX = 20.0f;
 	float barY = 20.0f;
 	float maxBarWidth = 200.0f;
 	float barHeight = 20.0f;
 
-	// 2.calculate the health ratio between (o.o and 1.0).
 	float healthRatio = player.current_health / player.max_health;
 
-	// 3. draw the background (Empty/missing health)
 	DrawRectangle(barX, barY, maxBarWidth, barHeight, DARKGRAY);
 
-	// 4.draw the foreround (currenthealth) scaled by the ratio.
 	DrawRectangle(barX, barY, maxBarWidth * healthRatio, barHeight, GREEN);
 
 	draw_player(player);
